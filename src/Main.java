b049b50f2d543dd8aab3564bd77e0f80d4503841
Kireenko Olga <kireenko86.osil@mail.ru>
2021-04-05 13:24:23 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+Public class Main {
+    public static void main (String[] args) {
+        int deposit = 2000_000_000;
+        int payment = 5000_000;
+        int total_deposit = deposit + payment;
+        System.out.println (total_deposit);
+    }
+}
