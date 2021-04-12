448e70894149bbc73f43e741b32ea189ea037085
Kireenko Olga <kireenko86.osil@mail.ru>
2021-04-12 13:28:49 +0500

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-Public class Main {
-    public static void main (String[] args) {
+public class Main {
+    public static void main(String[] args) {
         int deposit = 2000_000_000;
-        int payment = 5000_000;
+        int payment = 500_000_000;
         int total_deposit = deposit + payment;
         System.out.println (total_deposit);
     }
